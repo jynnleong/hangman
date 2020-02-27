89744cb8edd4b65d3de06bca9e0189cab6cc7f56
jynnleong <60396911+jynnleong@users.noreply.github.com>
2020-02-27 13:48:33 -0330

    Update GetWord.java

--- GetWord.java
+++ GetWord.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * This is a class that generates random words from the text file that's been included.
+ */
 public class GetWord
 {
     private List<String> words;
@@ -19,6 +22,7 @@ public class GetWord
         this.actualword = "";
         this.usedwords = new HashSet<>();
 
+        //Reading the file containing the words to guess 
         File file = new File("/Users/jynnleong/Desktop/Coding practice/Hangman java/Words.txt");
 
         try(BufferedReader b = new BufferedReader(new FileReader(file)))
@@ -34,15 +38,19 @@ public class GetWord
             e.printStackTrace();
         }
 
+        //Randomly retrieve a word from the .txt file
         this.word = words.get(rand.nextInt(words.size()));
         
+        // The while loop ensures that the same word won't be used as the word to guess 
         while(usedwords.contains(this.word))
         {
             this.word = words.get(rand.nextInt(words.size()));
         }
         
+        //If the word has not yet been used, it will be added into the HashSet to ensure no duplication
         this.usedwords.add(this.word);
 
+        // This for loop is to make sure the length of the word is the same as the blank string displayed on screen
         for(int i = 0; i < this.word.length(); i++)
         {
             this.actualword = this.actualword + Character.toString(this.word.charAt(i)) + " ";
@@ -50,6 +58,7 @@ public class GetWord
 
         ArrayList<String> wordlist = new ArrayList<>();
 
+        // The for loop converts the word to be guessed to "_" 
         for(int i = 0; i < this.word.length(); i++)
         {
             wordlist.add("_");
@@ -58,6 +67,7 @@ public class GetWord
 
         StringBuilder s = new StringBuilder();
 
+        // The StringBuilder then turns the elements from the "wordlist" array into a string
         for(int i = 0; i < wordlist.size(); i++)
         {
             s.append(wordlist.get(i));
